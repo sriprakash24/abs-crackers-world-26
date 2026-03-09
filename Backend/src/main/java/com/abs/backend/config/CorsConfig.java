f27e83e84a085ee647e86f1d29c680dd3589d19c
sriprakash24 <76991404+sriprakash24@users.noreply.github.com>
2026-03-10 03:07:17 +0530

    Update CorsConfig.java

--- Backend/src/main/java/com/abs/backend/config/CorsConfig.java
+++ Backend/src/main/java/com/abs/backend/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://abs-crackers-world-26.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 
