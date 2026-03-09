739a217732f5b6652bd04d18ecd2bb026b30aa44
sriprakash24 <76991404+sriprakash24@users.noreply.github.com>
2026-03-10 03:03:25 +0530

    Update api.js

--- abs-frontend/src/api/api.js
+++ abs-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://abs-crackers-world-26.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
